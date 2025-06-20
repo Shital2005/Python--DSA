1b58ffc73f43e62e59d5d09a8a6ab6f1f7899630
Shital2005 <shitalvijayprajapati85@gmail.com>
2025-06-20 23:26:54 +0530

    print.py

---
+++ print.py
@@ -0,0 +1,12 @@
+line1 = "Twinkle, twinkle, little star,"
+line2 = "    How I wonder what you are!"
+line3 = "        Up above the world so high,"
+line4 = "        Like a diamond in the sky."
+
+# Print using logic
+print(line1)
+print(line2)
+print(line3)
+print(line4)
+print(line1)
+print(line2)
